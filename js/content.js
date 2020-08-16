ba38df8c160c2726fef0dc8cf1916e7e0a783a60
shion.t.fujie <tonaluna32966@gmail.com>
2020-08-16 16:45:49 +0900

    addNewPage

--- js/content.js
+++ js/content.js
@@ -3,5 +3,11 @@ chrome.runtime.onMessage.addListener(({ type }) => {
 });
 
 function addNewPage() {
-    console.debug("trying to add new page")
+  console.debug("trying to add new page");
+  const button = $("div[role='button']:has(div:contains(New page))");
+  if (button == null) {
+      console.debug("Cannot find button to add new page")
+      return
+  }
+  button.click()
 }
