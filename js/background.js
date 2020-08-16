5d46ea681e4f81eb131845799c4eb3b367363e82
shion.t.fujie <tonaluna32966@gmail.com>
2020-08-16 16:31:56 +0900

    expose public api

--- js/background.js
+++ js/background.js
@@ -1,1 +1,28 @@
-console.debug("Background.js")
+chrome.runtime.onMessageExternal.addListener((request, _, response) => {
+  if (request.type === "action spec") {
+    response({
+      name: actionSpec.name,
+      actions: Object.entries(actionSpec.actions).map(
+        ([name, { displayName }]) => {
+          return { name, displayName };
+        }
+      )
+    });
+  } else if (request.type === "execute action") {
+    const action = actionSpec.actions[request.action.name];
+    if (action !== undefined) action.f();
+  }
+});
+
+const actionSpec = {
+  name: "Notion",
+  actions: {
+    [TYPE_ADD_NEW_PAGE]: {
+      f: requestAddNewPage
+    }
+  }
+};
+
+function requestAddNewPage() {
+    sendMessageToActiveTab({type: TYPE_ADD_NEW_PAGE})
+}
--- js/content.js
+++ js/content.js
@@ -1,1 +1,7 @@
-console.debug("Hello, SHION!")
+chrome.runtime.onMessage.addListener(({ type }) => {
+  if (type === TYPE_ADD_NEW_PAGE) addNewPage();
+});
+
+function addNewPage() {
+    console.debug("trying to add new page")
+}
